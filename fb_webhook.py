f73982b0ad26259a7b28f0caa55dd6c0c4bb6550
grepto <grepto@gmail.com>
2019-09-26 10:59:27 +0300

    add send_menu()

--- fb_webhook.py
+++ fb_webhook.py
@@ -61,5 +61,37 @@ def send_message(recipient_id, message_text):
     )
     response.raise_for_status()
 
+
+def send_menu(recipient_id):
+    params = {"access_token": FACEBOOK_TOKEN}
+    headers = {"Content-Type": "application/json"}
+    request_content = {
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "button",
+                    "text": "Try the postback button!",
+                    "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "Postback Button",
+                            "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    response = requests.post(
+        "https://graph.facebook.com/v2.6/me/messages",
+        params=params, headers=headers, json=request_content
+    )
+    response.raise_for_status()
+
+
 if __name__ == '__main__':
     app.run(debug=True)
